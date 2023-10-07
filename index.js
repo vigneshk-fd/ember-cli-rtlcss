0db74a3f93716a47785a38747030b946aee09dec
Vignesh Kumar <vignesh.kumar@freshworks.com>
2023-10-07 23:26:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,29 @@
 /* jshint node: true */
 'use strict';
-//var debug = require('broccoli-stew').debug;
 var RTLRewrite = require('./broccoli-rtl-filter');
 var BroccoliMergeTrees = require('broccoli-merge-trees');
 
 module.exports = {
   name: 'ember-cli-rtlcss',
-  isDevelopingAddon: function() {
+	isDevelopingAddon: function () {
     return true;
-  },
-  initializeOptions: function() {
-    this.options = {
-      enabled: true //this.app.env === 'production',
-    };
-  },
+	},
+	included: function (app) {
+    this.options = app.options[this.name] || {};
 
-  postprocessTree: function (type, inputNodes) {
-    if (type === 'css' ) {
-      //console.info('ember-cli-rtlcss:postprocessTree', type);
-      var rtlNodes = new RTLRewrite(inputNodes);
-      var merged = new BroccoliMergeTrees([inputNodes, rtlNodes], {overwrite:true});
-      return merged;
-      //return debug(merged, {name:'css-tree'});
-    }else{
-      return inputNodes;
+    if (!('enabled' in this.options)) {
+      this.options.enabled = true;
     }
   },
+	postprocessTree: function (type, inputNodes) {
+		var options = this.options || {};
 
-  included: function (app) {
-    this.app = app;
-    this.initializeOptions();
-  },
+		if (type === 'css' && options.enabled) {
+			var rtlNodes = new RTLRewrite(inputNodes);
+			var merged = new BroccoliMergeTrees([inputNodes, rtlNodes], { overwrite: true });
+			return merged;
+    } else {
+      return inputNodes;
+    }
+  }
 };
